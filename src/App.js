58be0b151ed7a644e5be1f2261eec82ced457bdf
ShakivAli <shakivaliamir123@gmail.com>
2023-11-17 10:31:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import {profileData} from './Helper';
+import {useState} from "react";
 
-function App() {
+const App = () => {
+
+  const [postVisible, setPostVisible] = useState(true);
+
+  const togglePostVisibility = () => {
+    setPostVisible(!postVisible);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="interface">
+      <div className="header">
+        <div className="leftpart">
+          <img src="https://www.terriblytinytales.com/img/home/ttt.svg" alt="logo"></img>
+          <p>STORIES</p>
+        </div>
+        <div className="rightpart">
+          <button style={{cursor:"pointer"}}>Courses</button>
+        </div>
+      </div>
+      <div className="middlesection">
+        <img src={profileData.bgImage} alt="background" className="bgImage"></img>
+        <div className="upper">
+          <img src={profileData.profileImage} alt="profile" className="profileImage"></img>
+          <div className="impdata">
+            <div className="name">
+              <p>{profileData.name}</p>
+              <img src="https://img.icons8.com/fluency/48/test-account--v1.png" alt="male" style={{width:"20px", height:"20px"}}></img>
+              <img src="https://img.icons8.com/color/48/000000/tiktok-verified-account.png" alt="verified" style={{width:"20px", height:"20px"}}></img>
+            </div>
+            <div className="follow">
+              <div className="followers">
+                <p>{profileData.followers}</p>
+                <p>Followers</p>
+              </div>
+              <div className="following">
+                <p>{profileData.following}</p>
+                <p>Following</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="middle">
+          <p>{profileData.bio}</p>
+          <p><a href={profileData.external}>{profileData.external}</a></p>
+          <div className="icons">
+            <img width="20" height="20" src="https://img.icons8.com/plasticine/100/star--v1.png" alt="star--v1"/>
+            <p>{profileData.star}</p>
+            <img width="20" height="20" src="https://img.icons8.com/emoji/48/thumbs-up.png" alt="thumbs-up"/>
+            <p>{profileData.likes}</p>
+            <img width="20" height="20" src="https://img.icons8.com/external-glyph-silhouettes-icons-papa-vector/78/external-Seen-interface-glyph-silhouettes-icons-papa-vector.png" alt="seen"/>
+            <p>{profileData.seen}</p>
+            <img width="20" height="20" src="https://img.icons8.com/fluency/48/like.png" alt="like"/>
+            <p>{profileData.heart}</p>
+          </div>
+        </div>
+      </div>
+        <div className="lower" onClick={togglePostVisibility}>
+          <p>{profileData.nofposts}</p>
+        </div>
+        {postVisible && (
+          <>
+            {profileData.posts.map(post => (
+              <div key={post.id} className="lowersection">
+                <div className="upperportion">
+                  <p className="title">{post.title}</p>
+                  <img width="20" height="20" src="https://img.icons8.com/emoji/48/thumbs-up.png" alt="thumbs-up"/>
+                </div>
+                <p className="content">{post.text}</p>
+                <div className="lowerportion">
+                  <div className="leftpart">
+                    <p>{post.signing}</p>
+                  </div>
+                  <div className="rightpart">
+                    <p>{post.date}</p>
+                    <p>~</p>
+                    <p>{post.read}</p>
+                    <p>~</p>
+                    <p>{post.views}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Helper.js
@@ -0,0 +1,43 @@
+export const profileData = {
+    bgImage: "https://i.pinimg.com/originals/e4/33/d5/e433d5515b9aa5834f6015bd9c3af28f.jpg",
+    profileImage: "https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D",
+    name: 'Anuj Gosalia',
+    followers: 6482,
+    following: 245,
+    bio: 'Co-founder & CEO at Terribly Tiny Tales',
+    external: 'http://www.instagram.com/anujgosalia',
+    star: 125,
+    likes: 12,
+    seen: '57.8k',
+    heart: '26.0k',
+    nofposts: '134 Posts',
+    posts: [
+        {
+            id: 1,
+            title: 'A Changing World Order', 
+            text: 'The world is changing at a feverish pace. Friends, colleagues and everyone engages in knowledge work are begining to sense this, including...',
+            signing: 'musing by anujgosalia',
+            date: 'August 2',
+            read: '2 mins Read',
+            views: '102 Views'
+        },
+        {
+            id: 2,
+            title: 'Indian v/s Australia', 
+            text: 'Think about it - this cricket series was akin to therapy. \n \n Laying our childhood trauma up top, working with it slowly and surely over two m...',
+            signing: 'thoughts by anujgosalia',
+            date: 'January 21',
+            read: '1 min Read',
+            views: '156 Views'
+        },
+        {
+            id: 3,
+            title: 'Write to Build',
+            text: 'Writing is the first step to create: \n\n -stories \n -products \n -companies',
+            signing: 'thoughts by anujgosalia',
+            date: 'November 18',
+            read: '1 min Read',
+            views: '228 Views'
+        },
+    ],
+}
